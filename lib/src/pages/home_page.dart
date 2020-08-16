40081c6b630ede01060243a795e655cfe0c95633
juan chan <juan_chan@MacBook-Pro-de-juan.local>
2020-08-16 18:12:46 -0500

    fixed movies and sliverTitleBar

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -6,93 +6,66 @@ import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 import 'package:peliculas/src/widgets/movie_horizontal.dart';
 
 class HomePage extends StatelessWidget {
-
   final peliculasProvider = new PeliculasProvider();
 
   @override
   Widget build(BuildContext context) {
-
     peliculasProvider.getPopulares();
 
-
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: false,
-        title: Text('Películas en cines'),
-        backgroundColor: Colors.indigoAccent,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon( Icons.search ),
-            onPressed: () {
-              showSearch(
-                context: context, 
-                delegate: DataSearch(),
-                // query: 'Hola'
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text('Películas en cines'),
+          backgroundColor: Colors.indigoAccent,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                showSearch(
+                  context: context,
+                  delegate: DataSearch(),
+                  // query: 'Hola'
                 );
-            },
-          )
-        ],
-      ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            _swiperTarjetas(),
-            _footer(context)
+              },
+            )
           ],
         ),
-      )
-       
-    );
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[_swiperTarjetas(), _footer(context)],
+          ),
+        ));
   }
 
   Widget _swiperTarjetas() {
-
     return FutureBuilder(
       future: peliculasProvider.getEnCines(),
       builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-        
-        if ( snapshot.hasData ) {
-          return CardSwiper( peliculas: snapshot.data );
+        if (snapshot.hasData) {
+          return CardSwiper(peliculas: snapshot.data);
         } else {
-          return Container(
-            height: 400.0,
-            child: Center(
-              child: CircularProgressIndicator()
-            )
-          );
+          return Container(height: 400.0, child: Center(child: CircularProgressIndicator()));
         }
-        
       },
     );
-
-
-
-    
-
-
   }
 
-
-  Widget _footer(BuildContext context){
-
+  Widget _footer(BuildContext context) {
     return Container(
       width: double.infinity,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
-            padding: EdgeInsets.only(left: 20.0),
-            child: Text('Populares', style: Theme.of(context).textTheme.subhead  )
-          ),
+              padding: EdgeInsets.only(left: 20.0),
+              child: Text('Populares', style: Theme.of(context).textTheme.subtitle1)),
           SizedBox(height: 5.0),
-
           StreamBuilder(
             stream: peliculasProvider.popularesStream,
             builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-              
-              if ( snapshot.hasData ) {
-                return MovieHorizontal( 
+              if (snapshot.hasData) {
+                return MovieHorizontal(
                   peliculas: snapshot.data,
                   siguientePagina: peliculasProvider.getPopulares,
                 );
@@ -101,12 +74,8 @@ class HomePage extends StatelessWidget {
               }
             },
           ),
-
         ],
       ),
     );
-
-
   }
-
 }
--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -6,40 +6,31 @@ import 'package:peliculas/src/models/pelicula_model.dart';
 import 'package:peliculas/src/providers/peliculas_provider.dart';
 
 class PeliculaDetalle extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-
     final Pelicula pelicula = ModalRoute.of(context).settings.arguments;
 
-
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          _crearAppbar( pelicula ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                SizedBox( height: 10.0 ),
-                _posterTitulo( context, pelicula ),
-                _descripcion( pelicula ),
-                _descripcion( pelicula ),
-                _descripcion( pelicula ),
-                _descripcion( pelicula ),
-                _crearCasting( pelicula )
-              ]
-            ),
-          )
-        ],
-      )
-    );
+        body: CustomScrollView(
+      slivers: <Widget>[
+        _crearAppbar(pelicula),
+        SliverList(
+          delegate: SliverChildListDelegate([
+            SizedBox(height: 10.0),
+            _posterTitulo(context, pelicula),
+            _descripcion(pelicula),
+            _descripcion(pelicula),
+            _descripcion(pelicula),
+            _descripcion(pelicula),
+            _crearCasting(pelicula)
+          ]),
+        )
+      ],
+    ));
   }
 
-
-  Widget _crearAppbar( Pelicula pelicula ){
-
-    return SliverAppBar(
+  Widget _crearAppbar(Pelicula pelicula) {
+    final sliverAppBar = SliverAppBar(
       elevation: 2.0,
       backgroundColor: Colors.indigoAccent,
       expandedHeight: 200.0,
@@ -52,18 +43,18 @@ class PeliculaDetalle extends StatelessWidget {
           style: TextStyle(color: Colors.white, fontSize: 16.0),
         ),
         background: FadeInImage(
-          image: NetworkImage( pelicula.getBackgroundImg() ),
+          image: NetworkImage(pelicula.getBackgroundImg()),
           placeholder: AssetImage('assets/img/loading.gif'),
-          fadeInDuration: Duration(microseconds: 150),
+          fadeInDuration: Duration(milliseconds: 250),
           fit: BoxFit.cover,
         ),
       ),
     );
 
+    return sliverAppBar;
   }
 
-  Widget _posterTitulo(BuildContext context, Pelicula pelicula ){
-
+  Widget _posterTitulo(BuildContext context, Pelicula pelicula) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 20.0),
       child: Row(
@@ -73,7 +64,7 @@ class PeliculaDetalle extends StatelessWidget {
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20.0),
               child: Image(
-                image: NetworkImage( pelicula.getPosterImg() ),
+                image: NetworkImage(pelicula.getPosterImg()),
                 height: 150.0,
               ),
             ),
@@ -83,12 +74,13 @@ class PeliculaDetalle extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text(pelicula.title, style: Theme.of(context).textTheme.title, overflow: TextOverflow.ellipsis ),
-                Text(pelicula.originalTitle, style: Theme.of(context).textTheme.subhead, overflow: TextOverflow.ellipsis ),
+                Text(pelicula.title, style: Theme.of(context).textTheme.subtitle1, overflow: TextOverflow.ellipsis),
+                Text(pelicula.originalTitle,
+                    style: Theme.of(context).textTheme.subtitle2, overflow: TextOverflow.ellipsis),
                 Row(
                   children: <Widget>[
-                    Icon( Icons.star_border ),
-                    Text( pelicula.voteAverage.toString(), style: Theme.of(context).textTheme.subhead )
+                    Icon(Icons.star_border),
+                    Text(pelicula.voteAverage.toString(), style: Theme.of(context).textTheme.subtitle2)
                   ],
                 )
               ],
@@ -97,12 +89,9 @@ class PeliculaDetalle extends StatelessWidget {
         ],
       ),
     );
-
   }
 
-
-  Widget _descripcion( Pelicula pelicula ) {
-
+  Widget _descripcion(Pelicula pelicula) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
       child: Text(
@@ -110,67 +99,53 @@ class PeliculaDetalle extends StatelessWidget {
         textAlign: TextAlign.justify,
       ),
     );
-
   }
 
-
-  Widget _crearCasting( Pelicula pelicula ) {
-
+  Widget _crearCasting(Pelicula pelicula) {
     final peliProvider = new PeliculasProvider();
 
     return FutureBuilder(
       future: peliProvider.getCast(pelicula.id.toString()),
       builder: (context, AsyncSnapshot<List> snapshot) {
-        
-        if( snapshot.hasData ) {
-          return _crearActoresPageView( snapshot.data );
+        if (snapshot.hasData) {
+          return _crearActoresPageView(snapshot.data);
         } else {
           return Center(child: CircularProgressIndicator());
         }
-
       },
     );
-
   }
 
-  Widget _crearActoresPageView( List<Actor> actores ) {
-
+  Widget _crearActoresPageView(List<Actor> actores) {
     return SizedBox(
       height: 200.0,
       child: PageView.builder(
         pageSnapping: false,
-        controller: PageController(
-          viewportFraction: 0.3,
-          initialPage: 1
-        ),
+        controller: PageController(viewportFraction: 0.3, initialPage: 1),
         itemCount: actores.length,
-        itemBuilder: (context, i) =>_actorTarjeta( actores[i] ),
+        itemBuilder: (context, i) => _actorTarjeta(actores[i]),
       ),
     );
-
   }
 
-  Widget _actorTarjeta( Actor actor ) {
+  Widget _actorTarjeta(Actor actor) {
     return Container(
-      child: Column(
-        children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: FadeInImage(
-              image: NetworkImage( actor.getFoto() ),
-              placeholder: AssetImage('assets/img/no-image.jpg'),
-              height: 150.0,
-              fit: BoxFit.cover,
-            ),
+        child: Column(
+      children: <Widget>[
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: FadeInImage(
+            image: NetworkImage(actor.getFoto()),
+            placeholder: AssetImage('assets/img/no-image.jpg'),
+            height: 150.0,
+            fit: BoxFit.cover,
           ),
-          Text(
-            actor.name,
-            overflow: TextOverflow.ellipsis,
-          )
-        ],
-      )
-    );
+        ),
+        Text(
+          actor.name,
+          overflow: TextOverflow.ellipsis,
+        )
+      ],
+    ));
   }
-
-
 }
